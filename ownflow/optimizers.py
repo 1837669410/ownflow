398841d8a17b9337dfedeeb2b0de909ad3e5cd51
1837669410 <1837669410@qq.com>
2024-12-03 19:12:28 +0800

    添加Adadelta、Adam、AdamW、Adamax优化器

--- ownflow/optimizers.py
+++ ownflow/optimizers.py
@@ -53,6 +53,95 @@ class Adagrad(Optimizer):
         self._init_t += 1
 
 
+class Adadelta(Optimizer):
+
+    def __init__(self, params, lr=1.0, rho=0.9, weight_decay=0, eps=1e-6):
+        super().__init__(params, lr)
+
+        self._rho = rho
+        self._weight_decay = weight_decay
+        self._eps = eps
+        self._v = [np.zeros_like(v) for v in self.vars]
+        self._u = [np.zeros_like(v) for v in self.vars]
+
+    def step(self):
+        for var, grad, v, u in zip(self.vars, self.grads, self._v, self._u):
+            if self._weight_decay != 0:
+                grad += self._weight_decay * var
+            v[:] = self._rho * v + (1 - self._rho) * grad ** 2
+            delta_x = np.sqrt(u + self._eps) / np.sqrt(v + self._eps) * grad
+            u[:] = self._rho * u + (1 - self._rho) * delta_x ** 2
+            var -= self._lr * delta_x
+
+
+class Adam(Optimizer):
+
+    def __init__(self, params, lr=0.001, betas=(0.9, 0.999), weight_decay=0, eps=1e-8):
+        super().__init__(params, lr)
+
+        self._beta1, self._beta2 = betas[0], betas[1]
+        self._weight_decay = weight_decay
+        self._eps = eps
+        self._init_t = 0
+        self._m = [np.zeros_like(v) for v in self.vars]
+        self._v = [np.zeros_like(v) for v in self.vars]
+
+    def step(self):
+        for var, grad, m, v in zip(self.vars, self.grads, self._m, self._v):
+            if self._weight_decay != 0:
+                grad += self._weight_decay * var
+            m[:] = self._beta1 * m + (1 - self._beta1) * grad
+            v[:] = self._beta2 * v + (1 - self._beta2) * grad ** 2
+            mt = m / (1 - self._beta1 ** (self._init_t + 1))
+            vt = v / (1 - self._beta2 ** (self._init_t + 1))
+            var -= self._lr * mt / (np.sqrt(vt) + self._eps)
+        self._init_t += 1
+
+
+class AdamW(Optimizer):
+
+    def __init__(self, params, lr=0.001, betas=(0.9, 0.999), weight_decay=0.01, eps=1e-8):
+        super().__init__(params, lr)
+
+        self._beta1, self._beta2 = betas[0], betas[1]
+        self._weight_decay = weight_decay
+        self._eps = eps
+        self._init_t = 0
+        self._m = [np.zeros_like(v) for v in self.vars]
+        self._v = [np.zeros_like(v) for v in self.vars]
+
+    def step(self):
+        for var, grad, m, v in zip(self.vars, self.grads, self._m, self._v):
+            var -= self._lr * self._weight_decay * var
+            m[:] = self._beta1 * m + (1 - self._beta1) * grad
+            v[:] = self._beta2 * v + (1 - self._beta2) * grad ** 2
+            mt = m / (1 - self._beta1 ** (self._init_t + 1))
+            vt = v / (1 - self._beta2 ** (self._init_t + 1))
+            var -= self._lr * mt / (np.sqrt(vt) + self._eps)
+        self._init_t += 1
+
+
+class Adamax(Optimizer):
+
+    def __init__(self, params, lr=0.002, betas=(0.9, 0.999), weight_decay=0, eps=1e-8):
+        super().__init__(params, lr)
+
+        self._beta1, self._beta2 = betas[0], betas[1]
+        self._weight_decay = weight_decay
+        self._eps = eps
+        self._m = [np.zeros_like(v) for v in self.vars]
+        self._u = [np.zeros_like(v) for v in self.vars]
+        self._init_t = 0
+
+    def step(self):
+        for var, grad, m, u in zip(self.vars, self.grads, self._m, self._u):
+            if self._weight_decay != 0:
+                grad += self._weight_decay * var
+            m[:] = self._beta1 * m + (1 - self._beta1) * grad
+            u[:] = np.maximum(self._beta2 * u, np.abs(grad) + self._eps)
+            var -= self._lr * m / (1 - self._beta1 ** (self._init_t + 1)) / u
+        self._init_t += 1
+
 
 if __name__ == '__main__':
 
